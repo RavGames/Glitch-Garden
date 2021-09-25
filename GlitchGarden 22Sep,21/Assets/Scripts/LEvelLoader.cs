2052161befbc5fba670cc0c2de856f5941c60733
RavGames <RaiderRickG@gmail.com>
2021-09-25 22:51:03 +0530

    Splash Screen And Level Loader
    Creating the Splash Screen and then loading the next Level from script.

---
+++ GlitchGarden 22Sep,21/Assets/Scripts/LEvelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LEvelLoader : MonoBehaviour
+{
+    [SerializeField] float waitTime = 4f;
+
+    private void Start()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if(currentSceneIndex == 0)
+        {
+            StartCoroutine(Delay());
+        }
+    }
+
+
+    private IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("StartGame");
+    }
+
+
+}//CLASS
