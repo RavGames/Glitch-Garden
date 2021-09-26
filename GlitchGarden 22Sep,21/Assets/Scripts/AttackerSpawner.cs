6212dcf42ddbf3671c9fcf5b6f4613b923bbd435
RavGames <RaiderRickG@gmail.com>
2021-09-26 14:18:41 +0530

    Adding Attacker Spanwer
    here we have created the array of attackers in attacker Spawner and instantiating them from it. eg:- Lizard and Fox

---
+++ GlitchGarden 22Sep,21/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] Attacker[] attackers;
+
+    [SerializeField] float minTime = 1f;
+    [SerializeField] float maxTime = 3f;
+    [SerializeField] bool spawn = true;
+
+
+    private IEnumerator Start()
+    {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+            Spawn();
+        }
+    }
+
+
+
+    private void SpawnAttackers(Attacker attacker)
+    {
+        Attacker newAttacker = Instantiate(attacker,
+                                transform.position,
+                                Quaternion.identity) as Attacker;
+    }
+
+
+    private void Spawn()
+    {
+        int attackerIndex = Random.Range(0, attackers.Length);
+        SpawnAttackers(attackers[attackerIndex]);
+    }
+
+    
+}//CLASS
