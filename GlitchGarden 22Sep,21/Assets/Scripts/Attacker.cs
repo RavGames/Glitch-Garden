3cc2905fa265eb8d28d11ff58448f46869a448ea
RavGames <RaiderRickG@gmail.com>
2021-09-26 11:02:58 +0530

    Adding Lizard as an attacker
    Creating the Lizard animations ( jump and walk ) and adding the attacker Script to it. And also using the animation event to walk the Lizard.

---
+++ GlitchGarden 22Sep,21/Assets/Scripts/Attacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    private float speed = 0f;
+
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    //this function will be called from animation event placed on the Lizard walk animation
+    private void SetMovementSpeed(float moveSpeed)
+    {
+        speed = moveSpeed;
+    }
+
+}//CLASS
