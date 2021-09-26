56efb2a68ea116407eb80b2ba5b79c1505fa2657
RavGames <RaiderRickG@gmail.com>
2021-09-26 15:01:12 +0530

    Defender Spawner
    Here we are Spawning the defender on Mouse Click.

---
+++ GlitchGarden 22Sep,21/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defenders;
+
+    private void OnMouseDown()
+    {
+        SpawnDefenders(GetMousePos());
+    }
+
+    private Vector2 GetMousePos()
+    {
+        Vector2 mousePos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Debug.Log("Current POs of mouse : " + mousePos);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector2 defenderPos = SnapToGrid(worldPos);
+        return defenderPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 gridPos)
+    {
+        float x = Mathf.RoundToInt(gridPos.x);
+        float y = Mathf.RoundToInt(gridPos.y);
+        return new Vector2(x, y);
+    }
+
+    private void SpawnDefenders(Vector2 pos)
+    {
+        GameObject gameObject = Instantiate(defenders,
+                                pos, 
+                                Quaternion.identity) as GameObject;
+    }
+
+}
