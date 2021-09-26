b93e2e33821c2b7de270772b416a3f59c0d109c8
RavGames <RaiderRickG@gmail.com>
2021-09-26 14:41:21 +0530

    Health And Projetcile's Damage
    Adding health to the attackers and some hit points(damage) to the Projectiles to do damage to the enemy.

---
+++ GlitchGarden 22Sep,21/Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float health = 100f;
+    [SerializeField] GameObject particleEffects;
+
+    public void ReduceHealth(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            ShowDeathEffect();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowDeathEffect()
+    {
+        GameObject vfx = Instantiate(particleEffects,
+                                    transform.position,
+                                    Quaternion.identity) as GameObject;
+
+        Destroy(gameObject,2f);
+    }
+
+
+
+
+}//CLASS
--- GlitchGarden 22Sep,21/Assets/Scripts/Projectile.cs
+++ GlitchGarden 22Sep,21/Assets/Scripts/Projectile.cs
@@ -6,13 +6,31 @@ public class Projectile : MonoBehaviour
 {
 
     [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
+
 
     private void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
     }
 
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var attacker = otherCollider.gameObject.GetComponent<Attacker>();
 
+        Health enemyHealth = otherCollider.gameObject.GetComponent<Health>();
+
+        if(attacker && enemyHealth)
+        {
+            enemyHealth.ReduceHealth(damage);
+            Destroy(gameObject);
+        }
+        else
+        {
+            return;
+        }
+        
+    }
 
 
 }//CLASS
